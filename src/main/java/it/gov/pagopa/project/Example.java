8f82ca2a9508c3c58e594f03e78da7f420683a2f
ciuffagianluca <gianluca.ciuffa@pagopa.it>
2025-04-16 18:40:04 +0200

    initial commit

--- src/main/java/it/gov/pagopa/project/Example.java
+++ src/main/java/it/gov/pagopa/project/Example.java
@@ -1,15 +1,10 @@
 package it.gov.pagopa.project;
 
 import com.microsoft.azure.functions.*;
-import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.FunctionName;
-import com.microsoft.azure.functions.annotation.HttpTrigger;
+import com.microsoft.azure.functions.annotation.TimerTrigger;
 
-import javax.ws.rs.core.MediaType;
 import java.time.LocalDateTime;
-import java.util.Optional;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * Azure Functions with Azure Queue trigger.
@@ -17,25 +12,13 @@ import java.util.logging.Logger;
 public class Example {
 
     /**
-     * This function will be invoked when a Http Trigger occurs
+     * This function will be invoked periodically according to the specified schedule.
      */
     @FunctionName("ExampleFunction")
-    public HttpResponseMessage run (
-            @HttpTrigger(
-                    name = "ExampleTrigger",
-                    methods = {HttpMethod.GET},
-                    route = "example",
-                    authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
-            final ExecutionContext context) {
-
-        Logger logger = context.getLogger();
-
-        String message = String.format("it.gov.pagopa.project.Example function called at: %s", LocalDateTime.now());
-        logger.log(Level.INFO, () -> message);
-
-        return request.createResponseBuilder(HttpStatus.OK)
-                .header("Content-Type", MediaType.TEXT_PLAIN)
-                .body(message)
-                .build();
+    public void run(
+            @TimerTrigger(name = "timerInfo", schedule = "0 * * * * *") String timerInfo,
+            final ExecutionContext context
+    ) {
+        context.getLogger().info("Java Timer trigger function executed at: " + LocalDateTime.now());
     }
 }
