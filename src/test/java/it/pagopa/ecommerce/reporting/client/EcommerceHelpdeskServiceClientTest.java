0203c3039b8f70180dcca81ddebf6dbc3ad2cad7
ciuffagianluca <gianluca.ciuffa@pagopa.it>
2025-04-24 19:53:09 +0200

    add helpdesk service client tests

--- src/test/java/it/pagopa/ecommerce/reporting/client/EcommerceHelpdeskServiceClientTest.java
+++ src/test/java/it/pagopa/ecommerce/reporting/client/EcommerceHelpdeskServiceClientTest.java
@@ -2,21 +2,34 @@
 package it.pagopa.ecommerce.reporting.client;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.microsoft.azure.functions.ExecutionContext;
 import it.pagopa.ecommerce.reporting.clients.EcommerceHelpdeskServiceClient;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junitpioneer.jupiter.SetEnvironmentVariable;
 import org.mockito.Mock;
+import org.mockito.MockedStatic;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.time.OffsetDateTime;
 import java.util.logging.Logger;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+@SetEnvironmentVariable(key = "HELPDESK_SERVICE_API_KEY", value = "API_KEY")
 @ExtendWith(MockitoExtension.class)
 public class EcommerceHelpdeskServiceClientTest {
 
@@ -26,6 +39,17 @@ public class EcommerceHelpdeskServiceClientTest {
     @Mock
     ExecutionContext context;
 
+    @Mock
+    private CloseableHttpClient httpClientMock;
+
+    @Mock
+    private CloseableHttpResponse httpResponseMock;
+
+    @Mock
+    private StatusLine statusLineMock;
+
+    private MockedStatic<HttpClients> mockStatic;
+
     EcommerceHelpdeskServiceClient ecommerceHelpdeskServiceClient;
 
     @Test
@@ -34,7 +58,7 @@ public class EcommerceHelpdeskServiceClientTest {
     }
 
     @Test
-    public void fetchTransactionMetricsTest() {
+    public void fetchTransactionMetricsTestNoClientHttp() {
         ecommerceHelpdeskServiceClient = new EcommerceHelpdeskServiceClient(mockLogger);
         JsonNode node = ecommerceHelpdeskServiceClient.fetchTransactionMetrics(
                 "clientId",
@@ -45,6 +69,46 @@ public class EcommerceHelpdeskServiceClientTest {
         );
         assertNotNull(node);
         assertTrue(node.isEmpty());
+    }
+
+    @Test
+    public void fetchTransactionMetricsTestNoValidData() {
+        mockStatic = mockStatic(HttpClients.class);
+        when(HttpClients.createDefault()).thenReturn(httpClientMock);
+        ecommerceHelpdeskServiceClient = new EcommerceHelpdeskServiceClient(mockLogger);
+        JsonNode node = ecommerceHelpdeskServiceClient.fetchTransactionMetrics(
+                null,
+                "pspId",
+                "paymentTypeCode",
+                OffsetDateTime.now(),
+                OffsetDateTime.now().minusHours(1)
+        );
+        assertNotNull(node);
+        assertTrue(node.isEmpty());
+        mockStatic.close();
+    }
+
+    @Test
+    public void testNodeFetch() throws IOException {
+        mockStatic = mockStatic(HttpClients.class);
+        when(HttpClients.createDefault()).thenReturn(httpClientMock);
+
+        when(httpClientMock.execute(any(HttpPost.class))).thenReturn(
+                httpResponseMock
+        );
+        when(httpResponseMock.getEntity()).thenReturn(new StringEntity("{\"field\":\"1\"}", StandardCharsets.UTF_8));
+
+        ecommerceHelpdeskServiceClient = new EcommerceHelpdeskServiceClient(mockLogger);
+        JsonNode node = ecommerceHelpdeskServiceClient.fetchTransactionMetrics(
+                "clientId",
+                "pspId",
+                "paymentTypeCode",
+                OffsetDateTime.now(),
+                OffsetDateTime.now().minusHours(1)
+        );
+        assertNotNull(node);
+        assertFalse(node.isEmpty());
+        mockStatic.close();
     }
 
 }
